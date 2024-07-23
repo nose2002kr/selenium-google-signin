f29d122684866b192dc6b04a578a30f82c11fc85
ksks <nose2002kr@gmail.com>
2024-07-23 12:46:28 +0900

    Move `main.py` into `test` directory

--- test/main.py
+++ test/main.py
@@ -1,5 +1,5 @@
-from src.google_login import login_to_google
-from src.google_login import launch_selenium
+from google_login import login_to_google
+from google_login import launch_selenium
 
 if __name__ == '__main__':
     driver = launch_selenium('chromedriver.exe')
